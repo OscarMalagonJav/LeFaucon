ec0867a72eea22435548d7e0348f12c8afcf9143
OscarMalagon <o.malagon@javeriana.edu.co>
2016-09-21 23:03:26 -0500

    Working on the boolean maps

--- Saliency/BooleanMap/BooleanMap.cxx
+++ Saliency/BooleanMap/BooleanMap.cxx
@@ -10,26 +10,18 @@ std::list<UnsignedCharImageType::Pointer> generateBooleanMaps(std::list<std::str
     std::list<UnsignedCharImageType::Pointer>::iterator itFeatures = listOfFeatureImages.begin();
     std::list<std::string>::iterator itThreshold = listOfThreshold.begin();
 
-    //The region assumes that the size of all the region's images is the same
-    UnsignedCharImageType::RegionType region = *itFeatures->GetLargestPossibleRegion(); 
-    UnsignedCharImageType::SizeType regionSize = region.GetSize();
-
     for(itThreshold; itThreshold != listOfThreshold.end(); itThreshold++)
     {
         for(itFeatures; itFeatures != listOfFeatureImages.end(); itFeatures++)
         {
-            ThresholdFilterType::Pointer thresholdFilter = FilterType::New();
-            thresholdFilter->SetInput( *itFeatures->GetOutput() );
-            thresholdFilter->SetUpperThreshold( *itThreshold );
-            thresholdFilter->SetOutsideValue( 255 );
-            thresholdFilter->SetInsideValue( 0 );
-            thresholdFilter->Update();
-            listOfBooleanMaps.insert(thresholdFilter->GetOutput());
+            UnsignedCharImageType::Pointer booleanImage        = thresholdingImage( *itThreshold, *itFeatures->GetOutput(), false);
+            UnsignedCharImageType::Pointer inverseBooleanImage = thresholdingImage( *itThreshold, *itFeatures->GetOutput(), true);
+            
+            //listOfBooleanMaps.insert(thresholdFilter->GetOutput());
         }
     }
     return listOfBooleanMaps;
 }
-
 
 std::list<UnsignedCharImageType::Pointer> loadFeatureImages(std::list<std::string> listOfFeatureImagesPath)
 {
@@ -45,17 +37,33 @@ std::list<UnsignedCharImageType::Pointer> loadFeatureImages(std::list<std::strin
     return listOfImages;
 }
 
-
 std::list<int> calculateListOfThresholds(int numberOfThresholds) {
     if(numberOfThresholds <= 0)
     {
         return NULL;
     }
     std::list<int> listOfThresholds;
-    int delta = floor(255/ numberOfThresholds);
+    int delta = floor(255 / numberOfThresholds);
     for (int index = 1; index <= numberOfThresholds; index++)
     {
         listOfThresholds.insert(delta*index);
     }
     return listOfThresholds;
+}
+
+UnsignedCharImageType::Pointer thresholdingImage(int thresholdValue, UnsignedCharImageType::Pointer inputImage, bool isNegative)
+{
+    ThresholdFilterType::Pointer thresholdFilter = FilterType::New();
+    thresholdFilter->SetInput( inputImage );
+    thresholdFilter->SetUpperThreshold( thresholdValue );
+    if(isNegative)
+    {
+        thresholdFilter->SetOutsideValue( 255 );
+        thresholdFilter->SetInsideValue( 0 );
+    }else{
+        thresholdFilter->SetOutsideValue( 0 );
+        thresholdFilter->SetInsideValue( 255 );
+    }
+    thresholdFilter->Update();
+    return thresholdImage->GetOutput();
 }
--- Saliency/BooleanMap/BooleanMap.h
+++ Saliency/BooleanMap/BooleanMap.h
@@ -6,5 +6,7 @@
 std::list<UnsignedCharImageType::Pointer> generateBooleanMaps(std::list<std::string> listOfFeatureImagesPath, int numberOfThresholds);
 std::list<UnsignedCharImageType::Pointer> loadFeatureImages(std::list<std::string> listOfFeatureImagesPath);
 std::list<int> calculateListOfThresholds(int numberOfThresholds);
+UnsignedCharImageType::Pointer thresholdingImage(int thresholdValue, UnsignedCharImageType::Pointer inputImage, bool isNegative);
+
 
 #endif
